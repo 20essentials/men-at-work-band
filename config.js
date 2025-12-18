24acb1dc9fb8c3fe0855d84ef8df5f1b275b6273
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:28:14 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MEN AT WORK';
+export let nameSongs = [
+	"DOWN UNDER", /*1*/
+	"WHO CAN IT BE NOW?", /*2*/
+	"OVERKILL", /*3*/
+	"IT'S A MISTAKE", /*4*/
+	"BE GOOD JOHNNY", /*5*/
+	"I CAN SEE IT IN YOUR EYES", /*6*/
+	"DOWN BY THE SEA", /*7*/
+	"NO SIGN OF YESTERDAY", /*8*/
+	"HARD LUCK STORY", /*9*/
+	"EVERYTHING I NEED", /*10*/
+	"MARIA", /*11*/
+	"STILL LIFE", /*12*/
+	"MAN WITH TWO HEARTS", /*13*/
+	"NO RESTRICTIONS", /*14*/
+	"UPSTAIRS IN MY HOUSE", /*15*/
+	"BLUE FOR YOU", /*16*/
+	"I LIKE TO", /*17*/
+	"CATCH A STAR", /*18*/
+	"PEOPLE JUST LOVE TO PLAY WITH WORDS", /*19*/
+	"UNDERGROUND", /*20*/
+]
